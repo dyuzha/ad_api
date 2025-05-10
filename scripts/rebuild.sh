2219e6b2ae2172bf702484561388fe8942170bc7
dyuzha <dyuzha@MacBook-Pro-dyuzha.local>
2025-05-10 14:48:50 +0300

    change rebuild script

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Определяем директорию, в которой находится этот скрипт
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+docker stop aac && docker rm aac
 
-# Останавливаем и удаляем контейнер, если он существует
-echo "--- Stopped container..."
-docker stop um || true
-echo "--- Removed container..."
-docker rm um || true
-
-# Создаем докер образ
-echo " --- docker build -t u_manager $SCRIPT_DIR/.."
-docker build -t u_manager $SCRIPT_DIR/..
-
-# Запускаем контейнер
-echo " --- docker run -d -p 82:8000 --name aac ad_api"
+./build.sh && ./run.sh
